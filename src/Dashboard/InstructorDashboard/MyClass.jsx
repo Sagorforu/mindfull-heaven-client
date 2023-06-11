30732fb0ecb601098d41b709e3edee6d88c0dc7a
sagorforu <freelancersagor18@gmail.com>
2023-06-11 22:37:19 +0600

    instructor class updated

--- src/Dashboard/InstructorDashboard/MyClass.jsx
+++ src/Dashboard/InstructorDashboard/MyClass.jsx
@@ -1,22 +1,64 @@
 import { Helmet } from "react-helmet-async";
 import PageTitle from "../../Components/PageTitle/PageTitle";
-// import useAxiosSecure from "../../Components/Hooks/useAxiosSecure";
-// import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../Components/Hooks/useAuth";
 import { useEffect, useState } from "react";
+import { useRef } from "react";
+import Swal from "sweetalert2";
 
 const MyClass = () => {
   const { user } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
   const [classes, setClasses] = useState([]);
+  const nameValue = useRef(null);
+  const seatsValue = useRef(null);
+  const priceValue = useRef(null);
 
   useEffect(() => {
     fetch(`http://localhost:5000/myClass/${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setClasses(data);
       });
   }, [user]);
+
+  const handleUpdate = (updateId) => {
+    const name = nameValue.current.value;
+    const seats = seatsValue.current.value;
+    const price = priceValue.current.value;
+    const updateData = {
+      name: name,
+      seats: parseFloat(seats),
+      price: parseFloat(price),
+    };
+    fetch(`http://localhost:5000/manageClass/update/${updateId}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "FeedBack added successfully!!!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleModalOpen = (id) => {
+    setUpdateId(id);
+    console.log(id);
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
@@ -49,6 +91,13 @@ const MyClass = () => {
               <p>Price: ${singleClass.price}</p>
               {/* TODO: enrolled students come from after payment  */}
               <p>Enrolled Students: {"0"}</p>
+              {singleClass.status === "denied" ? (
+                <p className="font-semibold">
+                  Feedback: {singleClass.feedback}
+                </p>
+              ) : (
+                <></>
+              )}
               <p>
                 Status:
                 {singleClass.status === "denied" ? (
@@ -62,9 +111,67 @@ const MyClass = () => {
                 )}
               </p>
               <div className="card-actions mt-5 flex justify-between">
-                <button className="bg-[#0A5403] hover:bg-[#0e8d02] text-white font-bold py-2 px-4 rounded">
+                <button
+                  onClick={() => handleModalOpen(singleClass._id)}
+                  className="bg-[#0A5403] hover:bg-[#0e8d02] text-white font-bold py-2 px-4 rounded"
+                >
                   Update
                 </button>
+                {isModalOpen && (
+                  <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className=" p-20 absolute inset-0"></div>
+                    <div className="bg-[#f3fff1] text-black shadow-sm  p-5 relative rounded-xl transition-transform transition-duration-[500ms]">
+                      <button
+                        className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        onClick={handleModalClose}
+                      >
+                        ✕
+                      </button>
+                      <h3 className="text-2xl text-center font-bold mb-4">
+                        Update Class
+                      </h3>
+                      <div>
+                        <h3 className="text-lg font-bold mb-2">Class Name</h3>
+                        <input
+                          className="w-[350px] md:w-[500px] px-2 py-3 rounded-lg shadow-md"
+                          placeholder="Update Class Name"
+                          type="text"
+                          ref={nameValue}
+                          id=""
+                        />
+                      </div>
+                      <div className="mt-6">
+                        <h3 className="text-lg font-bold mb-2">Update Seats</h3>
+                        <input
+                          className="w-[350px] md:w-[500px] px-2 py-3 rounded-lg shadow-md"
+                          placeholder="Update Seats Number"
+                          type="number"
+                          ref={seatsValue}
+                          id=""
+                        />
+                      </div>
+                      <div className="mt-6">
+                        <h3 className="text-lg font-bold mb-2">Update Price</h3>
+                        <input
+                          className="w-[350px] md:w-[500px] px-2 py-3 rounded-lg shadow-md"
+                          placeholder="Update Price"
+                          type="text"
+                          ref={priceValue}
+                          id=""
+                        />
+                      </div>
+                      <br />
+                      <div className="text-right">
+                        <button
+                          onClick={() => handleUpdate(updateId)}
+                          className="bg-[#0A5403] hover:bg-[#0e8d02] text-white font-bold py-2 px-4 rounded mt-3"
+                        >
+                          Update Class
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
