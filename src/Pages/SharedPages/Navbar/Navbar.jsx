93c3cb5452dfae12f1e9340bf4e7a61982df16ae
sagorforu <freelancersagor18@gmail.com>
2023-06-17 16:59:53 +0600

    dark and light toggle done

--- src/Pages/SharedPages/Navbar/Navbar.jsx
+++ src/Pages/SharedPages/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
 import { FaBars } from "react-icons/fa";
+import { HiSun } from "react-icons/hi";
 import { MdOutlineLogout } from "react-icons/md";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../../../Components/Hooks/useAuth";
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
   const { user, logOut } = useAuth();
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
 
   const toggleOpen = () => {
     setOpen(!isOpen);
@@ -17,6 +27,13 @@ const Navbar = () => {
     logOut()
       .then(() => {})
       .catch((error) => console.log(error));
+  };
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
   };
 
   return (
@@ -60,6 +77,28 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-end">
+        <div className="me-4">
+          <label className="swap swap-rotate">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" onChange={handleToggle} />
+
+            {/* sun icon */}
+
+            <HiSun
+              className="swap-on fill-current w-10 h-10"
+              viewBox="0 0 24 24"
+            ></HiSun>
+
+            {/* moon icon */}
+            <svg
+              className="swap-off fill-current w-10 h-10"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            </svg>
+          </label>
+        </div>
         {user ? (
           <div className="text-lg font-bold mr-3">
             <Link to="/dashboard">Dashboard</Link>
